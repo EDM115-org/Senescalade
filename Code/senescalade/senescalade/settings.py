5cf66961d1bf69acb87c2786d6a178266fce93df
EDM115 <dev@edm115.eu.org>
2023-11-10 10:33:40 +0100

    structure

--- Code/senescalade/senescalade/settings.py
+++ Code/senescalade/senescalade/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -47,6 +48,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "users",
+    "admin",
+    "mail",
+    "insctiption",
 ]
 
 ROOT_URLCONF = "senescalade.urls"
@@ -121,6 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Tab-Magiques
+
+[![DeepSource](https://app.deepsource.com/gh/EDM115-org/Tab-Magiques.svg/?label=active+issues&show_trend=true&token=WF6hxNEht95_hyiFpZdVK2h6)](https://app.deepsource.com/gh/EDM115-org/Tab-Magiques/)
+
+## Code
+
+### Structure
+
+**Apps** :  
+- `users` : Connexion des utilisateurs, création de compte, file d'attente, modification d'infos, ...
+- `admin` : Portail admin, ...
+- `mail` : Gestion de l'envoi des mails
+- `inscription` : Gestion globale de l'inscription, des files d'attentes, des créneaux, ...
