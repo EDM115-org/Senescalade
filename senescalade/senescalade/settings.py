73127c4b15c1c6648491c787d482ae0232025bba
EDM115 <dev@edm115.eu.org>
2023-11-17 10:06:58 +0100

    homepage

--- senescalade/senescalade/settings.py
+++ senescalade/senescalade/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = "senescalade.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -126,10 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- senescalade/senescalade/urls.py
+++ senescalade/senescalade/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from senescalade.views import home_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("admins/", include("admins.urls")),
+	path("inscription/", include("inscription.urls")),
+	path("mail/", include("mail.urls")),
+	path("users/", include("users.urls")),
+	path('', home_view, name='home'),
 ]
