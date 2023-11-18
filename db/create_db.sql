4f5318acd0b30b573b8a23cc92201cc628ccb4d7
EDM115 <dev@edm115.eu.org>
2023-11-18 11:01:11 +0100

    fixes SQL-L003, SQL-L052, SQL-L039

--- db/create_db.sql
+++ db/create_db.sql
@@ -79,7 +79,7 @@ BEGIN
     IF NEW.DateNaissance > NOW() THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'La date de naissance ne peut pas être ultérieure à la date actuelle.';
-    END IF;
-END;
+    END IF;  -- skipcq: SQL-L003
+END;  -- skipcq: SQL-L003
 //
-DELIMITER ;
+DELIMITER ;  -- skipcq: SQL-L052, SQL-L039
