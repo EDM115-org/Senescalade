3bafaf1774d3331ff91f51f1244efd5ea55d2ce8
EDM115 <dev@edm115.eu.org>
2023-11-30 17:50:30 +0100

    fr

--- senescalade/inscription/forms.py
+++ senescalade/inscription/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 from .models import CustomUser
 
 
@@ -11,6 +12,12 @@ class CustomUserCreationForm(forms.ModelForm):
             'email': forms.EmailInput(),
             'password': forms.PasswordInput(),
             'confirm_password': forms.PasswordInput(),
+        }
+        labels = {
+            'birth_date': _('Date de naissance'),
+            'email': _('Email'),
+            'password': _('Mot de passe'),
+            'confirm_password': _('Confirmez le mot de passe'),
         }
 
 class CustomUserLoginForm(forms.Form):
