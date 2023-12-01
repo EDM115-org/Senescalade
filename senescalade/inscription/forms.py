d02bc90106445da858e016211b7b0f7c4f84d8aa
EDM115 <dev@edm115.eu.org>
2023-12-01 14:44:31 +0100

    fixes PY-D0002

--- senescalade/inscription/forms.py
+++ senescalade/inscription/forms.py
@@ -4,6 +4,24 @@ from .models import CustomUser
 
 
 class CustomUserCreationForm(forms.ModelForm):
+    """
+    A form for creating a new user.
+
+    Inherits from forms.ModelForm and provides fields and widgets for user registration.
+
+    Attributes:
+        birth_date (DateInput): The user's birth date.
+        email (EmailInput): The user's email address.
+        password (PasswordInput): The user's password.
+        confirm_password (PasswordInput): The confirmation of the user's password.
+
+    Labels:
+        birth_date: "Date de naissance"
+        email: "Email"
+        password: "Mot de passe"
+        confirm_password: "Confirmez le mot de passe"
+    """
+
     class Meta:
         model = CustomUser
         fields = ["birth_date", "email", "password", "confirm_password"]
@@ -22,5 +40,9 @@ class CustomUserCreationForm(forms.ModelForm):
 
 
 class CustomUserLoginForm(forms.Form):
+    """
+    A form for user login with email and password fields.
+    """
+
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
--- senescalade/models.py
+++ senescalade/models.py
@@ -1,14 +1,20 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
 
 class Admin(models.Model):
+    """
+    Model definition for the Admin model
+
+    Fields:
+        idadmin (AutoField): Primary key
+        droit (CharField): Droit d'administration
+        lapersonne (ForeignKey): Foreign key to Personne
+    
+    Meta:
+        managed (bool): If False, no database table creation, modification, or deletion operations will be performed for this model
+        db_table (str): The name of the database table to use for the model
+    """
+
     idadmin = models.AutoField(db_column="idAdmin", primary_key=True)
     droit = models.CharField(max_length=5)
     lapersonne = models.ForeignKey(
@@ -21,6 +27,17 @@ class Admin(models.Model):
 
 
 class AuthGroup(models.Model):
+    """
+    Model definition for the AuthGroup model
+
+    Fields:
+        name (CharField): Name of the group
+    
+    Meta:
+        managed (bool): If False, no database table creation, modification, or deletion operations will be performed for this model
+        db_table (str): The name of the database table to use for the model
+    """
+
     name = models.CharField(unique=True, max_length=150)
 
     class Meta:
@@ -29,6 +46,20 @@ class AuthGroup(models.Model):
 
 
 class AuthGroupPermissions(models.Model):
+    """
+    Model definition for the AuthGroupPermissions model
+
+    Fields:
+        id (AutoField): Primary key
+        group (ForeignKey): Foreign key to AuthGroup
+        permission (ForeignKey): Foreign key to AuthPermission
+
+    Meta:
+        managed (bool): If False, no database table creation, modification, or deletion operations will be performed for this model
+        db_table (str): The name of the database table to use for the model
+        unique_together (tuple): Tuple of fields that must be unique together
+    """
+
     id = models.BigAutoField(primary_key=True)
     group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
     permission = models.ForeignKey("AuthPermission", models.DO_NOTHING)
@@ -40,6 +71,20 @@ class AuthGroupPermissions(models.Model):
 
 
 class AuthPermission(models.Model):
+    """
+    Model definition for the AuthPermission model
+
+    Fields:
+        name (CharField): Name of the permission
+        content_type (ForeignKey): Foreign key to DjangoContentType
+        codename (CharField): Codename of the permission
+
+    Meta:
+        managed (bool): If False, no database table creation, modification, or deletion operations will be performed for this model
+        db_table (str): The name of the database table to use for the model
+        unique_together (tuple): Tuple of fields that must be unique together
+    """
+
     name = models.CharField(max_length=255)
     content_type = models.ForeignKey("DjangoContentType", models.DO_NOTHING)
     codename = models.CharField(max_length=100)
@@ -51,6 +96,26 @@ class AuthPermission(models.Model):
 
 
 class AuthUser(models.Model):
+    """
+    Model definition for the AuthUser model
+
+    Fields:
+        password (CharField): Hashed password
+        last_login (DateTimeField): Last login
+        is_superuser (IntegerField): Designates that this user has all permissions without explicitly assigning them
+        username (CharField): Username
+        first_name (CharField): First name
+        last_name (CharField): Last name
+        email (CharField): Email address
+        is_staff (IntegerField): Designates whether the user can access the admin site
+        is_active (IntegerField): Designates whether this user should be treated as active
+        date_joined (DateTimeField): Date joined
+
+    Meta:
+        managed (bool): If False, no database table creation, modification, or deletion operations will be performed for this model
+        db_table (str): The name of the database table to use for the model
+    """
+
     password = models.CharField(max_length=128)
     last_login = models.DateTimeField(blank=True, null=True)
     is_superuser = models.IntegerField()
@@ -68,6 +133,20 @@ class AuthUser(models.Model):
 
 
 class AuthUserGroups(models.Model):
+    """
+    Model definition for the AuthUserGroups model
+
+    Fields:
+        id (AutoField): Primary key
+        user (ForeignKey): Foreign key to AuthUser
+        group (ForeignKey): Foreign key to AuthGroup
+
+    Meta:
+        managed (bool): If False, no database table creation, modification, or deletion operations will be performed for this model
+        db_table (str): The name of the database table to use for the model
+        unique_together (tuple): Tuple of fields that must be unique together
+    """
+
     id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(AuthUser, models.DO_NOTHING)
     group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
@@ -79,6 +158,20 @@ class AuthUserGroups(models.Model):
 
 
 class AuthUserUserPermissions(models.Model):
+    """
+    Model definition for the AuthUserUserPermissions model
+
+    Fields:
+        id (AutoField): Primary key
+        user (ForeignKey): Foreign key to AuthUser
+        permission (ForeignKey): Foreign key to AuthPermission
+
+    Meta:
+        managed (bool): If False, no database table creation, modification, or deletion operations will be performed for this model
+        db_table (str): The name of the database table to use for the model
+        unique_together (tuple): Tuple of fields that must be unique together
+    """
+
     id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(AuthUser, models.DO_NOTHING)
     permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
@@ -90,6 +183,23 @@ class AuthUserUserPermissions(models.Model):
 
 
 class DjangoAdminLog(models.Model):
+    """
+    Model definition for the DjangoAdminLog model
+
+    Fields:
+        action_time (DateTimeField): Action time
+        object_id (TextField): Object id
+        object_repr (CharField): Object representation
+        action_flag (PositiveSmallIntegerField): Action flag
+        change_message (TextField): Change message
+        content_type (ForeignKey): Foreign key to DjangoContentType
+        user (ForeignKey): Foreign key to AuthUser
+
+    Meta:
+        managed (bool): If False, no database table creation, modification, or deletion operations will be performed for this model
+        db_table (str): The name of the database table to use for the model
+    """
+
     action_time = models.DateTimeField()
     object_id = models.TextField(blank=True, default='')
     object_repr = models.CharField(max_length=200)
@@ -106,6 +216,20 @@ class DjangoAdminLog(models.Model):
 
 
 class DjangoContentType(models.Model):
+    """
+    Model definition for the DjangoContentType model
+
+    Fields:
+        app_label (CharField): App label
+        model (CharField): Model
+        name (CharField): Name
+
+    Meta:
+        managed (bool): If False, no database table creation, modification, or deletion operations will be performed for this model
+        db_table (str): The name of the database table to use for the model
+        unique_together (tuple): Tuple of fields that must be unique together
+    """
+
     app_label = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
 
@@ -116,6 +240,20 @@ class DjangoContentType(models.Model):
 
 
 class DjangoMigrations(models.Model):
+    """
+    Model definition for the DjangoMigrations model
+
+    Fields: 
+        id (AutoField): Primary key
+        app (CharField): App
+        name (CharField): Name
+        applied (DateTimeField): Applied
+
+    Meta:
+        managed (bool): If False, no database table creation, modification, or deletion operations will be performed for this model
+        db_table (str): The name of the database table to use for the model
+    """
+
     id = models.BigAutoField(primary_key=True)
     app = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
@@ -127,6 +265,19 @@ class DjangoMigrations(models.Model):
 
 
 class DjangoSession(models.Model):
+    """
+    Model definition for the DjangoSession model
+
+    Fields:
+        session_key (CharField): Primary key
+        session_data (TextField): Session data
+        expire_date (DateTimeField): Expire date
+
+    Meta:
+        managed (bool): If False, no database table creation, modification, or deletion operations will be performed for this model
+        db_table (str): The name of the database table to use for the model
+    """
+
     session_key = models.CharField(primary_key=True, max_length=40)
     session_data = models.TextField()
     expire_date = models.DateTimeField()
@@ -137,6 +288,20 @@ class DjangoSession(models.Model):
 
 
 class Inscription(models.Model):
+    """
+    Model definition for the Inscription model
+
+    Fields:
+        idinscription (AutoField): Primary key
+        mail (CharField): Email address
+        password (CharField): Password
+        datenaissance (DateField): Date of birth
+
+    Meta:   
+        managed (bool): If False, no database table creation, modification, or deletion operations will be performed for this model
+        db_table (str): The name of the database table to use for the model
+    """
+
     idinscription = models.AutoField(db_column="idInscription", primary_key=True)
     mail = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
@@ -148,6 +313,21 @@ class Inscription(models.Model):
 
 
 class InscriptionCustomuser(models.Model):
+    """
+    Model definition for the InscriptionCustomuser model
+
+    Fields:
+        id (AutoField): Primary key
+        birth_date (DateField): Date of birth
+        email (CharField): Email address
+        password (CharField): Password
+        confirm_password (CharField): Password confirmation
+
+    Meta:
+        managed (bool): If False, no database table creation, modification, or deletion operations will be performed for this model
+        db_table (str): The name of the database table to use for the model
+    """
+
     id = models.BigAutoField(primary_key=True)
     birth_date = models.DateField()
     email = models.CharField(unique=True, max_length=254)
@@ -160,6 +340,44 @@ class InscriptionCustomuser(models.Model):
 
 
 class Personne(models.Model):
+    """
+    Model definition for the Personne model
+
+    Fields:
+        idpersonne (AutoField): The unique identifier for the person.
+        action (CharField): The action associated with the person.
+        nom (CharField): The last name of the person.
+        prenom (CharField): The first name of the person.
+        sexe (CharField): The gender of the person.
+        nationalite (CharField): The nationality of the person.
+        adresse (CharField): The address of the person.
+        complementadresse (CharField): The additional address information of the person.
+        codepostal (CharField): The postal code of the person.
+        ville (CharField): The city of the person.
+        pays (CharField): The country of the person.
+        telephone (CharField): The telephone number of the person.
+        mobile (CharField): The mobile number of the person.
+        courriel2 (CharField): The secondary email address of the person.
+        personnenom (CharField): The last name of the contact person.
+        personneprenom (CharField): The first name of the contact person.
+        personnetelephone (CharField): The telephone number of the contact person.
+        personnecourriel (CharField): The email address of the contact person.
+        numlicence (CharField): The license number of the person.
+        typelicence (CharField): The type of license of the person.
+        assurance (CharField): The insurance information of the person.
+        optionski (IntegerField): The ski option of the person.
+        optionslackline (IntegerField): The slackline option of the person.
+        optiontrail (IntegerField): The trail option of the person.
+        optionvtt (IntegerField): The mountain biking option of the person.
+        optionassurance (IntegerField): The insurance option of the person.
+        seance (ForeignKey): The foreign key to the associated Seance object.
+        linscription (ForeignKey): The foreign key to the associated Inscription object.
+
+    Meta:
+        managed (bool): If False, no database table creation, modification, or deletion operations will be performed for this model
+        db_table (str): The name of the database table to use for the model
+    """
+
     idpersonne = models.AutoField(db_column="idPersonne", primary_key=True)
     action = models.CharField(max_length=1)
     nom = models.CharField(max_length=100)
@@ -209,6 +427,25 @@ class Personne(models.Model):
 
 
 class Seance(models.Model):
+    """
+    Model definition for the Seance model
+
+    Fields:
+        idseance (AutoField): Primary key
+        jour (CharField): Day of the week
+        heureseance (TimeField): Time of the session
+        dureeseance (TimeField): Duration of the session
+        typeseance (CharField): Type of session
+        niveau (CharField): Level of the session
+        nbplaces (IntegerField): Number of places
+        nbplacesrestantes (IntegerField): Number of remaining places
+        professeur (CharField): Teacher of the session
+
+    Meta:
+        managed (bool): If False, no database table creation, modification, or deletion operations will be performed for this model
+        db_table (str): The name of the database table to use for the model
+    """
+
     idseance = models.AutoField(db_column="idSeance", primary_key=True)
     jour = models.CharField(max_length=50)
     heureseance = models.TimeField(db_column="heureSeance")
