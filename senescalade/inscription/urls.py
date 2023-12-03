c94f8c9527b2c26fe8fb8b4a3e57b11112d1a550
EDM115 <dev@edm115.eu.org>
2023-12-03 14:20:10 +0100

    fixes FLK-W191, FLK-E131

--- senescalade/inscription/urls.py
+++ senescalade/inscription/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path("", views.register_user, name="register"),
+    path("", views.register_user, name="register"),
     path("register/", views.register_user, name="register"),
     path("login/", views.login_user, name="login")
 ]
