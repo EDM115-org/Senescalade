3fd78489905bbc9446f9339ff0f8a4083bfe7273
EDM115 <dev@edm115.eu.org>
2023-12-08 15:05:47 +0100

    factorisation de code

--- senescalade/static/js/main.js
+++ senescalade/static/js/main.js
@@ -72,3 +72,135 @@ function minutesToTimeString(minutes) {
         return `${hours}h${remainingMinutes < 10 ? '0' : ''}${remainingMinutes}`;
     }
 }
+
+const dayNameToIndex = {
+    "Dimanche": 0,
+    "Lundi": 1,
+    "Mardi": 2,
+    "Mercredi": 3,
+    "Jeudi": 4,
+    "Vendredi": 5,
+    "Samedi": 6
+};
+const currentDate = new Date();
+const currentDayIndex = currentDate.getDay();
+
+function createEvent(idSeance, instance) {
+    var desiredDayIndex = dayNameToIndex[instance.jour];
+    var daysDifference = desiredDayIndex - currentDayIndex;
+    var originalDaysDifference = Math.abs(daysDifference);
+    var desiredDate = new Date(currentDate);
+    
+    if (desiredDayIndex == currentDayIndex) {
+        // do nothing
+    } else if (currentDayIndex == 0) {
+        daysDifference -= 7;
+    } else if (originalDaysDifference == 6) {
+        daysDifference += 7;
+    } else if (desiredDayIndex == 0 && daysDifference < 0) {
+        daysDifference += 7;
+    }
+
+    desiredDate.setDate(currentDate.getDate() + daysDifference);
+    var formattedDate = desiredDate.toISOString().slice(0, 10);
+
+    var event = {
+        id: idSeance,
+        title: instance.typeSeance,
+        start: instance.heureSeance,
+        end: instance.dureeSeance,
+        allDay: false,
+        extendedProps: {
+            nbPlaces: instance.nbPlaces,
+            nbPlacesRestantes: instance.nbPlacesRestantes,
+            professeur: instance.professeur,
+            dureeSeance: instance.dureeSeance,
+        },
+    };
+    if (instance.niveau != '') {
+        event.title += ' - ' + instance.niveau;
+    }
+    if (instance.nbPlacesRestantes > 0) {
+        event.backgroundColor = '#44475A';
+        event.textColor = '#F8F8F2';
+    } else {
+        event.backgroundColor = '#FF5555';
+        event.textColor = '#282A36';
+    }
+    event.start = formattedDate + 'T' + event.start;
+    event.end = formattedDate + 'T' + event.end;
+
+    return event;
+}
+
+function eventClick(info, document) {
+    if (info.event.extendedProps.nbPlacesRestantes <= 0) {
+        return;
+    }
+    document.getElementById("eventDetails").innerText = 
+        info.event.title + ", " +
+        info.event.extendedProps.jourSeance + ", " +
+        info.event.start.toLocaleTimeString("fr-FR", { hour: "2-digit", minute: "2-digit" }) + ", " +
+        "Durée: " + minutesToTimeString(calculateDurationInMinutes(info.event.start.toLocaleTimeString("fr-FR", { hour: "2-digit", minute: "2-digit" }), info.event.extendedProps.dureeSeance)) + ", " +
+        "Places: " + info.event.extendedProps.nbPlaces + ", " +
+        "Restantes: " + info.event.extendedProps.nbPlacesRestantes;
+    if (selectedEventId) {
+        var oldEvent = calendar.getEventById(selectedEventId);
+        if (oldEvent) {
+            oldEvent.setProp('backgroundColor', '#44475A');
+            oldEvent.setProp('textColor', '#F8F8F2');
+        }
+    }
+    info.event.setProp('backgroundColor', '#50FA7B');
+    info.event.setProp('textColor', '#282A36');
+    selectedEventId = info.event.id;
+    document.getElementById("selectedEventId").value = selectedEventId;
+    document.getElementById("eventForm").style.display = "";
+}
+
+function eventMouseEnter(info, document) {
+    if (info.event.extendedProps.nbPlacesRestantes > 0) {
+        info.el.style.cursor = "pointer";
+    } else {
+        return;
+    }
+    var tooltip = document.createElement("div");
+    tooltip.style.position = "absolute";
+    tooltip.style.top = info.jsEvent.pageY + "px";
+    tooltip.style.left = info.jsEvent.pageX + "px";
+    tooltip.style.backgroundColor = "#282A36";
+    tooltip.style.color = "#F8F8F2";
+    tooltip.style.padding = "5px";
+    tooltip.style.borderRadius = "5px";
+    tooltip.style.zIndex = "1000";
+    tooltip.innerText = 
+        info.event.title + ", " +
+        info.event.extendedProps.jourSeance + ", " +
+        info.event.start.toLocaleTimeString("fr-FR", { hour: "2-digit", minute: "2-digit" }) + ", " +
+        "Durée: " + minutesToTimeString(calculateDurationInMinutes(info.event.start.toLocaleTimeString("fr-FR", { hour: "2-digit", minute: "2-digit" }), info.event.extendedProps.dureeSeance)) + ", " +
+        "Places: " + info.event.extendedProps.nbPlaces + ", " +
+        "Restantes: " + info.event.extendedProps.nbPlacesRestantes;
+    document.body.appendChild(tooltip);
+    info.el.addEventListener("mouseleave", function() {
+        try {
+            if (tooltip && tooltip.parentNode) {
+                tooltip.parentNode.removeChild(tooltip);
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    });
+}
+
+function checkAllEventsFull() {
+    var allFull = true;
+    calendar.getEvents().forEach(function(event) {
+        if (event.extendedProps.nbPlacesRestantes > 0) {
+            allFull = false;
+        }
+    });
+
+    if (allFull) {
+        document.getElementById("waitlistForm").style.display = "";
+    }
+}
