2ef70e0374e0cff7f7213b28ab8fe458ed423f91
EDM115 <dev@edm115.eu.org>
2024-04-04 15:57:26 +0200

    super basic nuxt3 + vuetify3 starter

---
+++ senescalade/nuxt.config.js
@@ -0,0 +1,22 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
+export default defineNuxtConfig({
+  build: {
+    transpile: ['vuetify'],
+  },
+  devtools: { enabled: true },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    }
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls
+      }
+    }
+  }
+})
--- senescalade/users/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class UsersConfig(AppConfig):
-    """AppConfig class for the 'users' app."""
-
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "users"
