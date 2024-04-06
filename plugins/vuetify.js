9012ebe2f92c1f003a9d1914a8a10cb4500f5f1d
Restyled.io <commits@restyled.io>
2024-04-06 14:30:17 +0000

    Restyled by prettier

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,45 +1,45 @@
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
 
-import {createVuetify} from 'vuetify'
-import {en, fr} from 'vuetify/locale'
+import { createVuetify } from "vuetify";
+import { en, fr } from "vuetify/locale";
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
     locale: {
-      locale: 'fr',
-      fallback: 'en',
-      messages: { fr, en }
+      locale: "fr",
+      fallback: "en",
+      messages: { fr, en },
     },
     theme: {
-      defaultTheme: 'dark',
+      defaultTheme: "dark",
       themes: {
         light: {
           dark: false,
           colors: {
-            primary: '#1976D2',
-            secondary: '#424242',
-            accent: '#82B1FF',
-            error: '#FF5252',
-            info: '#2196F3',
-            success: '#4CAF50',
-            warning: '#FFC107'
-          }
+            primary: "#1976D2",
+            secondary: "#424242",
+            accent: "#82B1FF",
+            error: "#FF5252",
+            info: "#2196F3",
+            success: "#4CAF50",
+            warning: "#FFC107",
+          },
         },
         dark: {
           dark: true,
           colors: {
-            primary: '#2196F3',
-            secondary: '#424242',
-            accent: '#82B1FF',
-            error: '#FF5252',
-            info: '#2196F3',
-            success: '#4CAF50',
-            warning: '#FFC107'
-          }
-        }
-      }
-    }
-  })
-  app.vueApp.use(vuetify)
-})
+            primary: "#2196F3",
+            secondary: "#424242",
+            accent: "#82B1FF",
+            error: "#FF5252",
+            info: "#2196F3",
+            success: "#4CAF50",
+            warning: "#FFC107",
+          },
+        },
+      },
+    },
+  });
+  app.vueApp.use(vuetify);
+});
