ecfefe627a72a380fd6a951613c8be129ee0280e
yamakajump <corentin.batard2003@gmail.com>
2024-06-11 00:23:34 +0200

    affichage des crénaux pannel admin

--- pages/admin/gestion-crenaux/index.vue
+++ pages/admin/gestion-crenaux/index.vue
@@ -2,20 +2,154 @@
   <v-container class="fillheight">
     <LayoutNavBarAdmin v-if="isLoading" />
     <div v-if="isLoading">
-      <h1 class="text-center mt-5 mb-5">
-        Connexion
-      </h1>
+      <v-row justify="center">
+        <v-col cols="12">
+          <v-card>
+            <v-card-title>
+              <h2>Gestion des séances</h2>
+            </v-card-title>
+            <v-card-text>
+              <v-simple-table>
+                <template #default>
+                  <thead>
+                    <tr>
+                      <th
+                        style="width: 10%;"
+                        class="text-center"
+                      >
+                        ID
+                      </th>
+                      <th
+                        style="width: 15%;"
+                        class="text-center"
+                      >
+                        Jour
+                      </th>
+                      <th
+                        style="width: 15%;"
+                        class="text-center"
+                      >
+                        Heure
+                      </th>
+                      <th
+                        style="width: 15%;"
+                        class="text-center"
+                      >
+                        Durée
+                      </th>
+                      <th
+                        style="width: 15%;"
+                        class="text-center"
+                      >
+                        Type
+                      </th>
+                      <th
+                        style="width: 15%;"
+                        class="text-center"
+                      >
+                        Niveau
+                      </th>
+                      <th
+                        style="width: 10%;"
+                        class="text-center"
+                      >
+                        Places
+                      </th>
+                      <th
+                        style="width: 10%;"
+                        class="text-center"
+                      >
+                        Places restantes
+                      </th>
+                      <th
+                        style="width: 15%;"
+                        class="text-center"
+                      >
+                        Professeur
+                      </th>
+                      <th
+                        style="width: 15%;"
+                        class="text-center"
+                      >
+                        Actions
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr
+                      v-for="seance in seances"
+                      :key="seance.idSeance"
+                    >
+                      <td class="text-center">
+                        {{ seance.idSeance }}
+                      </td>
+                      <td class="text-center">
+                        {{ seance.jour }}
+                      </td>
+                      <td class="text-center">
+                        {{ seance.heureSeance }}
+                      </td>
+                      <td class="text-center">
+                        {{ seance.dureeSeance }}
+                      </td>
+                      <td class="text-center">
+                        {{ seance.typeSeance }}
+                      </td>
+                      <td class="text-center">
+                        {{ seance.niveau }}
+                      </td>
+                      <td class="text-center">
+                        {{ seance.nbPlaces }}
+                      </td>
+                      <td class="text-center">
+                        {{ seance.nbPlacesRestantes }}
+                      </td>
+                      <td class="text-center">
+                        {{ seance.professeur }}
+                      </td>
+                      <td class="text-center">
+                        <v-btn color="primary">
+                          Modifier
+                        </v-btn>
+                        <v-btn color="primary">
+                          Supprimer
+                        </v-btn>
+                      </td>
+                    </tr>
+                  </tbody>
+                </template>
+              </v-simple-table>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
     </div>
   </v-container>
 </template>
 
 <script setup>
+import { ref, onMounted } from "vue"
 import { useMainStore } from "~/store/main"
 import { useRouter } from "vue-router"
 
 const store = useMainStore()
 const router = useRouter()
 const isLoading = ref(false)
+const seances = ref([])
+
+const fetchSeance = async () => {
+  try {
+    const result = await $fetch("/api/fetchSeance")
+
+    if (result.status === 200) {
+      seances.value = result.body
+    } else {
+      console.error("Error fetching seances:", result)
+    }
+  } catch (error) {
+    console.error("Error fetching seances:", error)
+  }
+}
 
 onMounted(() => {
   const user = store.getUser
@@ -25,6 +159,7 @@ onMounted(() => {
       router.push("/user")
     } else {
       isLoading.value = true
+      fetchSeance()
     }
   } else {
     router.push("/login")
