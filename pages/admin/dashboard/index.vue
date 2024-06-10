957f82660b6611dabe4a1448e444487b80103764
yamakajump <corentin.batard2003@gmail.com>
2024-06-10 23:23:16 +0200

    protection pannel admin

--- pages/admin/dashboard/index.vue
+++ pages/admin/dashboard/index.vue
@@ -1,6 +1,33 @@
 <template>
   <v-container class="fillheight">
-    <LayoutNavBarAdmin />
+    <LayoutNavBarAdmin v-if="isLoading" />
+    <div v-if="isLoading">
+      <h1 class="text-center mt-5 mb-5">
+        Connexion
+      </h1>
+    </div>
   </v-container>
 </template>
 
+<script setup>
+import { useMainStore } from "~/store/main"
+import { useRouter } from "vue-router"
+
+const store = useMainStore()
+const router = useRouter()
+const isLoading = ref(false)
+
+onMounted(() => {
+  const user = store.getUser
+
+  if (user) {
+    if (user.isAdmin !== 1) {
+      router.push("/user")
+    } else {
+      isLoading.value = true
+    }
+  } else {
+    router.push("/login")
+  }
+})
+</script>
--- pages/admin/liste-grimpeurs/index.vue
+++ pages/admin/liste-grimpeurs/index.vue
@@ -1,6 +1,32 @@
 <template>
   <v-container class="fillheight">
-    <LayoutNavBarAdmin />
-    <CardUser />
+    <LayoutNavBarAdmin v-if="isLoading" />
+    <div v-if="isLoading">
+      <CardUser />
+    </div>
   </v-container>
 </template>
+
+<script setup>
+import { useMainStore } from "~/store/main"
+import { useRouter } from "vue-router"
+
+const store = useMainStore()
+const router = useRouter()
+const isLoading = ref(false)
+
+onMounted(() => {
+  const user = store.getUser
+
+  if (user) {
+    if (user.isAdmin !== 1) {
+      router.push("/user")
+    } else {
+      isLoading.value = true
+    }
+  } else {
+    router.push("/login")
+  }
+})
+</script>
+
