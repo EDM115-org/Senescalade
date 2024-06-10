3cb53167f99b2f6438db828c8eb5a0db603ed3a3
yamakajump <corentin.batard2003@gmail.com>
2024-06-10 23:37:59 +0200

    gestion admin affichage des admin fonctionnel

--- pages/admin/gestion-admin/index.vue
+++ pages/admin/gestion-admin/index.vue
@@ -2,20 +2,91 @@
   <v-container class="fillheight">
     <LayoutNavBarAdmin v-if="isLoading" />
     <div v-if="isLoading">
-      <h1 class="text-center mt-5 mb-5">
-        Connexion
-      </h1>
+      <v-row justify="center">
+        <v-col cols="12">
+          <v-card>
+            <v-card-title>
+              <h2>Gestion des administrateurs</h2>
+            </v-card-title>
+            <v-card-text>
+              <v-simple-table>
+                <template #default>
+                  <thead>
+                    <tr>
+                      <th
+                        style="width: 33%;"
+                        class="text-center"
+                      >
+                        Id
+                      </th>
+                      <th
+                        style="width: 33%;"
+                        class="text-center"
+                      >
+                        Email
+                      </th>
+                      <th
+                        style="width: 34%;"
+                        class="text-center"
+                      >
+                        Actions
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr
+                      v-for="admin in admins"
+                      :key="admin.idInscription"
+                    >
+                      <td class="text-center">
+                        {{ admin.idInscription }}
+                      </td>
+                      <td class="text-center">
+                        {{ admin.mail }}
+                      </td>
+                      <td class="text-center">
+                        <v-btn color="primary">
+                          Modifier
+                        </v-btn>
+                        <v-btn color="primary">
+                          Supprimer
+                        </v-btn>
+                      </td>
+                    </tr>
+                  </tbody>
+                </template>
+              </v-simple-table>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
     </div>
   </v-container>
 </template>
 
 <script setup>
+import { ref, onMounted } from "vue"
 import { useMainStore } from "~/store/main"
 import { useRouter } from "vue-router"
 
 const store = useMainStore()
 const router = useRouter()
 const isLoading = ref(false)
+const admins = ref([])
+
+const fetchAdmin = async () => {
+  try {
+    const result = await $fetch("/api/fetchAdmin")
+
+    if (result.status === 200) {
+      admins.value = result.body
+    } else {
+      console.error("Error fetching admins:", result)
+    }
+  } catch (error) {
+    console.error("Error fetching admins:", error)
+  }
+}
 
 onMounted(() => {
   const user = store.getUser
@@ -25,6 +96,7 @@ onMounted(() => {
       router.push("/user")
     } else {
       isLoading.value = true
+      fetchAdmin()
     }
   } else {
     router.push("/login")
