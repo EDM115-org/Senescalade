a30fe738a9b498b8019f12467c07d1f47f989310
corentin <corentin.batard2003@gmail.com>
2024-06-12 17:44:32 +0200

    nouvelle page user modificcation

---
+++ pages/user/profil.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-container class="fillheight">
+    <h1 class="text-center mt-5 mb-5">
+      Vous êtes connecté {{ mail }}
+    </h1>
+    <FormUpdatePassword />
+    <v-row class="justify-center">
+      <v-col cols="auto">
+        <v-btn
+          color="accent"
+          icon="mdi-pencil"
+          @click.prevent="confirmEdit(user)"
+        />
+      </v-col>
+      <v-col cols="auto">
+        <v-btn
+          color="error"
+          icon="mdi-delete"
+          @click.prevent="confirmDelete(user)"
+        />
+      </v-col>
+    </v-row>
+    <PopUpDeleteUser
+      ref="deleteDialog"
+      @confirm-delete="handleDelete"
+    />
+  </v-container>
+</template>
+
+<script setup>
+import { useMainStore } from "~/store/main"
+import { computed } from "vue"
+
+const store = useMainStore()
+const deleteDialog = ref(null)
+const mail = computed(() => store.getUser.mail)
+const user = store.getUser
+
+const errorMessage = ref("")
+const issueMessage = ref("")
+
+const confirmDelete = (user) => {
+  deleteDialog.value.open(user)
+}
+
+const handleDelete = (user) => {
+  deleteUser(user)
+}
+
+const deleteUser = async (id) => {
+  try {
+    const result = await $fetch("/api/deleteUser", {
+      method: "DELETE",
+      body: { idInscription: id }
+    })
+
+    if (result.status === 200) {
+      // redirection vers la page d'accueil
+      router.push("/")
+    } else {
+      errorMessage.value = result.body.error
+      issueMessage.value = result.body.message ?? ""
+    }
+  } catch (error) {
+    errorMessage.value = "Erreur lors de la suppression d'un utilisateur"
+    issueMessage.value = error
+  }
+}
+</script>
