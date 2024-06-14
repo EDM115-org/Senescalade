6d5259834dd42cf4b2fe93c2085342e625f2acdc
Allan Maccrez <allan.maccrez@gmail.com>
2024-06-14 17:03:37 +0200

    feat: dashboard améliorer

--- pages/admin/dashboard/index.vue
+++ pages/admin/dashboard/index.vue
@@ -7,11 +7,46 @@
       <h1 class="text-center mt-5 mb-5">
         Dashboard
       </h1>
+      <v-row>
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <v-card>
+            <v-card-title class="text-center">
+              Nombre de Grimpeurs
+            </v-card-title>
+            <v-card-text class="text-center">
+              <v-icon size="56">
+                mdi-account-group
+              </v-icon>
+              <h3>{{ grimpeurCount }}</h3>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <v-card>
+            <v-card-title class="text-center">
+              Personnes n'ayant pas payé
+            </v-card-title>
+            <v-card-text class="text-center">
+              <v-icon size="56">
+                mdi-currency-eur-off
+              </v-icon>
+              <h3>{{ nonPayersCount }}</h3>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
     </div>
   </v-container>
 </template>
 
 <script setup>
+import { ref, onMounted } from "vue"
 import { useMainStore } from "~/store/main"
 
 definePageMeta({
@@ -27,8 +62,38 @@ definePageMeta({
 const store = useMainStore()
 const router = useRouter()
 const adminLogged = ref(false)
+const grimpeurCount = ref(0)
+const nonPayersCount = ref(0)
 
-onMounted(() => {
+const fetchGrimpeurCount = async () => {
+  try {
+    const result = await $fetch("/api/countGrimpeur")
+
+    if (result.status === 200) {
+      grimpeurCount.value = result.body.personneCount
+    } else {
+      console.error("Error fetching grimpeur count:", result)
+    }
+  } catch (error) {
+    console.error("Error fetching grimpeur count:", error)
+  }
+}
+
+const fetchNonPayersCount = async () => {
+  try {
+    const result = await $fetch("/api/countNonPayers")
+
+    if (result.status === 200) {
+      nonPayersCount.value = result.body.nonPayersCount
+    } else {
+      console.error("Error fetching non-payers count:", result)
+    }
+  } catch (error) {
+    console.error("Error fetching non-payers count:", error)
+  }
+}
+
+onMounted(async () => {
   const user = store.getUser
 
   if (user) {
@@ -36,6 +101,8 @@ onMounted(() => {
       router.push("/user")
     } else {
       adminLogged.value = true
+      fetchGrimpeurCount()
+      fetchNonPayersCount()
     }
   } else {
     router.push("/login")
