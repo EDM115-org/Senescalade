00e3e9c02ded593e2ac9a268f3b978ccf5b1c102
EDM115 <dev@edm115.eu.org>
2024-06-22 12:54:04 +0200

    feat: redirection vers la page d'accueil si une route n'existe pas/en cas d'erreur non gérée

---
+++ error.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-main>
+      <LayoutNavBar />
+      <v-container
+        class="fill-height"
+      >
+        <v-row
+          align="center"
+          align-content="center"
+          justify="center"
+        >
+          <v-col align="center">
+            <NuxtLink to="/">
+              Redirection vers la page d'accueil...
+            </NuxtLink>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { onMounted } from "vue"
+
+onMounted(() => {
+  const router = useRouter()
+
+  router.push("/")
+})
+</script>
