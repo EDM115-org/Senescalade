e7347e2d1a5781be73174ef6014e128fe5aee725
EDM115 <git@edm115.eu.org>
2024-10-05 14:20:35 +0200

    chore(fix): Use concise character class syntax '\d' instead of '[0-9]'.

--- assets/utils/i18n-validators.js
+++ assets/utils/i18n-validators.js
@@ -10,9 +10,9 @@ export function createI18nValidators(t) {
   })
 
   return {
-    duree: withI18nMessage(validators.helpers.regex("duree", /^([01]?[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$/)),
+    duree: withI18nMessage(validators.helpers.regex("duree", /^([01]?\d|2[0-3]):([0-5]\d):([0-5]\d)$/)),
     email: withI18nMessage(validators.email),
-    heure: withI18nMessage(validators.helpers.regex("heure", /^([01]?[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$/)),
+    heure: withI18nMessage(validators.helpers.regex("heure", /^([01]?\d|2[0-3]):([0-5]\d):([0-5]\d)$/)),
     maxLength: withI18nMessage(validators.maxLength, { withArguments: true }),
     minLength: withI18nMessage(validators.minLength, { withArguments: true }),
     numeric: withI18nMessage(validators.numeric),
